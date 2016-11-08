f3491e812ebabc371efc9e4647c9c73d1c7773aa
Sam Alexander <sama93536@gmail.com>
2016-11-08 15:19:37 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //'Repository Pattern'
+            int[] testCodes = new int[9] { 62102, 61022, 61023, 61021, 61123, 61027, 61025, 62100, 62101 };
+            int[] testCodes2 = new int[11] { 65102, 65022, 65023, 65021, 65024, 65027, 65025, 65100, 65101, 65111, 65112 };
+
+            //Instantiate Regions
+            Region test = new Region(testCodes, "Region 1");
+            Region test2 = new Region(testCodes2, "Region 2");
+
+            //Test Output Region 1
+            Console.WriteLine("All Codes of " + test.getName());
+            foreach (int code in test.getAll())
+            {
+                Console.WriteLine(code);
+            }
+
+            Console.WriteLine("Individual groups of " + test.getName());
+            for (int i = 0; i < test.getGroupAmt(); i++)
+            {
+                Console.WriteLine("Group Number: " + i);
+
+                foreach (int code in test.getGroup(i))
+                {
+                    Console.WriteLine(code);
+                }
+            }
+
+            //Region 2
+            Console.WriteLine("All Codes of " + test2.getName());
+            foreach (int code in test2.getAll())
+            {
+                Console.WriteLine(code);
+            }
+
+            Console.WriteLine("Individual groups of " + test2.getName());
+            for (int i = 0; i < test2.getGroupAmt(); i++)
+            {
+                Console.WriteLine("Group Number: " + i);
+
+                foreach (int code in test2.getGroup(i))
+                {
+                    Console.WriteLine(code);
+                }
+            }
+
+            //Optional for Output reading purposes
+            System.Threading.Thread.Sleep(8000);
+        }
+    }
+}
---
+++ Region.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Region
+    {
+        private string Name;
+        private int GroupAmt = 0;
+        private List<List<int>> Groups = new List<List<int>>();
+
+        public Region(int[] codes, string name)
+        {
+            Name = name;
+
+            //Receive & Sort Codes
+            Array.Sort(codes);
+
+            int i = 0;
+            int prevCode = 0;
+            
+            List<int> curGroup = new List<int>();
+
+            //Add the code to a group 
+            foreach (int code in codes)
+            {
+                //If consecutive add code to current group, else create new group
+                if (code == prevCode + 1 || prevCode == 0)
+                {
+                    curGroup.Add(code);
+                }
+                else
+                {
+                    Groups.Add(curGroup);
+                    curGroup = new List<int>();
+                    curGroup.Add(code);
+                    
+                }
+                prevCode = code;
+                i++;
+            }
+            //Add the final group
+            Groups.Add(curGroup);
+
+            GroupAmt = Groups.Count;
+        }
+
+        public int[] getAll()
+        {
+            //Flatten Nested Lists
+            //I assumed you would want arrays as return types
+            return Groups.SelectMany(a => a).ToArray();
+        }
+
+        public int[] getGroup(int groupNo)
+        {
+            return Groups[groupNo].ToArray();
+        }
+
+        public string getName()
+        {
+            return Name;
+        }
+
+        public int getGroupAmt()
+        {
+            return GroupAmt;
+        }
+
+    }
+}
